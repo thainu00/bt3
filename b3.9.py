68b054558ba10b9ff4c8d7ffc9359f9d40e9909f
thainu00 <62654090+thainu00@users.noreply.github.com>
2020-04-28 21:16:29 +0700

    Add files via upload

---
+++ b3.9.py
@@ -0,0 +1,10 @@
+"""program make a simple calculator that can add, subtract, multiply and divide using function"""
+ #this function adds two numbers
+def add(x,y):
+    return x+y
+#this function subtracts  two numbers
+def subtract(x,y):
+    return x-y
+#this function multiplics two numbers
+def multiply(x,y):
+    
